282dea5da55c0ae1922baaa5070ff923ab39ded1
Yulia-kom <85898864+Yulia-kom@users.noreply.github.com>
2021-11-15 21:03:55 +0200

    task7

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,22 @@
-const input = document.getElementById("font-size-control");
-const span = document.getElementById("text");
+// const input = document.getElementById("font-size-control");
+// const span = document.getElementById("text");
 
-span.style.fontSize = input.value + 'px';
+// span.style.fontSize = input.value + 'px';
 
-input.addEventListener("input", (event) => {
-    span.style.fontSize = event.currentTarget.value + 'px';
-});
+// input.addEventListener("input", (event) => {
+//     span.style.fontSize = event.currentTarget.value + 'px';
+// });
+
+
+//второй вариант
+
+const inputEl = document.getElementById('font-size-control');
+const spanEl = document.getElementById('text');
+
+inputEl.addEventListener("input", onRangeInput);
+
+
+function onRangeInput() {
+
+    spanEl.style.fontSize = `${inputEl.value + 'px'}`;
+};
